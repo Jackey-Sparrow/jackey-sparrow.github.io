05eeee6fb0e458ea2ea0f05dd4172dee712f0345
Jackey <jackey.li@foxmail.com>
2015-08-18 22:11:12 +0800

    test min.js
    fail, need debug

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+/**
+ * Created by Jackey Li on 2015/8/18.
+ */
+
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    clean = require('gulp-clean'),
+    rimraf = require('gulp-rimraf'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
+
+
+var jsPaths = [
+    'www/js/global.js',
+    'www/js/app.js',
+
+
+
+    'www/js/platform/*.js',
+
+    'www/js/language/services/*.js',
+    'www/js/language/controllers/*.js',
+
+    'www/js/login/*.js',
+
+    'www/js/tweet/services/*.js',
+    'www/js/tweet/controllers/*.js',
+
+    'www/js/contacts/services/*.js',
+    'www/js/contacts/controllers/*.js',
+
+    'www/js/setting/services/*.js',
+    'www/js/setting/controllers/*.js'
+
+
+];
+
+gulp.task('buildJs', function () {
+    return gulp.src(jsPaths)
+        .pipe(uglify())
+        .pipe(concat('main.min.js'))
+        .pipe(gulp.dest('www/build'));
+});
+
+
+gulp.task('clean', function () {
+    return gulp.src(['www/build/*.js'], {read: false})
+        .pipe(rimraf({force: true}));
+});
+
+
+gulp.task('default', ['clean','buildJs']);
