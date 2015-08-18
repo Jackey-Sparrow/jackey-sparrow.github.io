be59a0701515d4c738281921ef44296ea881785b
Jackey <jackey.li@foxmail.com>
2015-08-18 21:10:23 +0800

    implement new version

--- www/js/tweet/controllers/tweet-detail-controller.js
+++ www/js/tweet/controllers/tweet-detail-controller.js
@@ -8,8 +8,10 @@
      * tweet detail controller
      */
     angular.module(globalSettings.appName).controller('tweetDetailController',
-        ['$scope', '$stateParams', 'tweetService','$ionicHistory','$state',
-            function ($scope, $stateParams, tweetService,$ionicHistory,$state) {
+        ['$scope', '$stateParams', 'tweetService', '$ionicHistory', '$state',
+            'tweetCommentService', '$ionicActionSheet', '$timeout',
+            function ($scope, $stateParams, tweetService, $ionicHistory, $state,
+                      tweetCommentService, $ionicActionSheet, $timeout) {
 
                 //tweet Id
                 var tweetId = $stateParams.tweetId;
@@ -20,6 +22,61 @@
                 //get tweet
                 $scope.tweet = tweetService.getTweetById(parseInt(tweetId));
 
+                //get comments
+                $scope.comments = [];
+                $scope.hasComments = $scope.comments.length;
+
+                /*
+                 * init: get the data
+                 */
+                var init = function () {
+                    $scope.tweet = tweetService.getTweetById(parseInt(tweetId));
+
+                    tweetCommentService.getCommentByTweetId($scope.tweet.id).then(function (comments) {
+                        $scope.comments = comments;
+                        $scope.hasComments = $scope.comments.length;
+                    }, function (error) {
+                        //todo
+                    });
+                };
+
+                /*
+                 * add comment
+                 */
+                $scope.addComment = function () {
+                    //todo
+                };
+
+                /*
+                 * reply comment
+                 */
+                $scope.replyComment = function () {
+
+                    // Show the action sheet
+                    var hideSheet = $ionicActionSheet.show({
+                        buttons: [
+                            {text: 'Reply'},
+                            {text: 'Copy'}
+                        ],
+                        //destructiveText: 'Delete',
+                        //titleText: 'Modify your album',
+                        cancelText: 'Cancel',
+                        cancel: function () {
+                            // add cancel code..
+                        },
+                        buttonClicked: function (index) {
+                            console.log(index);
+                            return true;
+                        }
+                    });
+
+                    // For example's sake, hide the sheet after two seconds
+                    $timeout(function () {
+                        hideSheet();
+                    }, 2000);
+
+                };
+
                 /*
                  * back
                  */
@@ -27,5 +84,8 @@
                     //$ionicHistory.goBack();
                     $state.go('tab.tweet');
                 };
+
+                //init the page
+                init();
             }]);
 })(angular);
---
+++ www/js/tweet/services/tweet-comment-service.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Jackey Li on 2015/8/18.
+ */
+(function (angular) {
+    'use strict';
+    angular.module(globalSettings.appName).factory('tweetCommentService',
+        ['$http', '$q',
+            function ($http, $q) {
+                var service = {},
+                    list = [];//may use data buffer, but now just let it be
+
+                service.getCommentByTweetId = function (tweetId) {
+                    var defer = $q.defer();
+                    $http.get('js/tweet/comments.json').then(function (response) {
+                        list = response.data;
+                        var result = list.filter(function (item) {
+                            return item.tweetId === tweetId;
+                        });
+                        defer.resolve(result);
+                    }, function (error) {
+                        //todo:show error
+                        console.log(error);
+                        defer.reject(error);
+                    });
+
+                    return defer.promise;
+
+                };
+
+
+                return service;
+            }]);
+})(angular);
