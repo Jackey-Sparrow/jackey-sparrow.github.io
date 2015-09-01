18245ad0ec58ec539a4cf88a5d27511550e9799f
Jackey.Sparrow <1109952026@qq.com>
2015-09-01 22:29:34 +0800

    add some docs

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,18 @@ var gulp = require('gulp'),
     minifyCss = require('gulp-minify-css'),
     ngMin = require('gulp-ngmin');
 
+//sass的编译 gulp-ruby-sass
+//自动添加css前缀 gulp-autoprefixer
+//压缩css  gulp-minify-css
+//js代码校验 gulp-jshint
+//合并js代码 gulp-concat
+//压缩js代码 gulp-uglify
+//压缩图片 gulp-imagemin
+//自动刷新页面 gulp-livereload
+//图片缓存，只有图片替换了才压缩gulp-cache
+//更改提醒 gulp-notify
+//清除文件 del
+
 var rootPath = 'ionic/';
 
 var cssPath = [
