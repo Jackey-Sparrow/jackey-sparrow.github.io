f8f6e55c88147c499c8de9af095d1d40735d0895
Jackey.Sparrow <1109952026@qq.com>
2015-12-02 23:51:48 +0800

    implement search

--- know-your-browser/desktop/controllers/desktop-controller.js
+++ know-your-browser/desktop/controllers/desktop-controller.js
@@ -5,12 +5,28 @@
 	'use strict'
 
 	angular.module('desktop').controller('desktopController',
-		['$scope', 'desktopDataService',
-		 function ($scope, desktopDataService) {
+		['$scope', 'desktopDataService', '$timeout',
+		 function ($scope, desktopDataService, $timeout) {
 			 $scope.name = 'desktop';
 
 			 $scope.collection = desktopDataService.getCollection();
 
+			 $scope.keyWord = '';
+
+			 $scope.keyWordChange = function () {
+				 var result = {};
+				 for (var key in $scope.collection) {
+					 if (key.indexOf($scope.keyWord) !== -1) {
+						 result[key] = $scope.collection[key];
+					 }
+				 }
+
+				 $scope.collection = result;
+				 $timeout(function () {
+					 $scope.$apply();
+				 });
+
+			 };
 
 		 }]);
 })(angular);
--- know-your-browser/desktop/directives/bom-in-body.js
+++ know-your-browser/desktop/directives/bom-in-body.js
@@ -13,30 +13,44 @@
 				},
 				link: function (scope, element, attr) {
 					var browsers = ['CHROME', 'SAFARI', 'FIREFOX', 'IE10', 'IE9', 'IE8', 'IE7', 'IE5'];
-					var html = '<table>';
-					html += '<tr>';
-					html += '<td></td>';
-					browsers.forEach(function (browser) {
-						html += '<td class="browserTitle">' + browser + '</td>';
-					});
-					html += '</tr>';
 
-					for (var item in scope.collection) {
-						html += '<tr>'
-						html += '<td>' + item + '</td>';
+					function build() {
+						element.empty();
+						var html = '<table>';
+						html += '<tr>';
+						html += '<td></td>';
 						browsers.forEach(function (browser) {
-							var index = scope.collection[item].indexOf(browser),
-								markUp = '';
-							if (~index) {
-								markUp = browser;
-							}
-							html += '<td class="browser">' + markUp + '</td>';
+							html += '<td class="browserTitle">' + browser + '</td>';
 						});
 						html += '</tr>';
-					}
-					html += '</table>';
 
-					element.append($compile(html)(scope));
+						for (var item in scope.collection) {
+							html += '<tr>'
+							html += '<td>' + item + '</td>';
+							browsers.forEach(function (browser) {
+								var index = scope.collection[item].indexOf(browser),
+									markUp = '';
+								if (~index) {
+									markUp = browser;
+								}
+								html += '<td class="browser">' + markUp + '</td>';
+							});
+							html += '</tr>';
+						}
+						html += '</table>';
+
+						element.append(html);
+					}
+
+					var unWatch = scope.$watch('collection', function () {
+						build();
+					});
+
+					scope.$on('destroy', function () {
+						unWatch();
+					});
+
+					build();
 				}
 			};
 		}]);
