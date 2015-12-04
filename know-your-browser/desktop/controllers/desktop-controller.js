acabd3459715caf1b8a468c5693769baf8d6f9ef
Jackey.Sparrow <1109952026@qq.com>
2015-12-04 23:19:01 +0800

    add up

--- know-your-browser/desktop/controllers/desktop-controller.js
+++ know-your-browser/desktop/controllers/desktop-controller.js
@@ -7,22 +7,26 @@
 	angular.module('desktop').controller('desktopController',
 		['$scope', 'desktopDataService', '$timeout',
 		 function ($scope, desktopDataService, $timeout) {
+
 			 $scope.name = 'desktop';
 
-			 desktopDataService.getBodyBom().then(function (data) {
-				 $scope.collection = data;
-				 $scope.collectionCopy = angular.copy($scope.collection);
-			 });
-
 			 $scope.keyWord = '';
+
+			 function init() {
+				 desktopDataService.getBodyBom().then(function (data) {
+					 $scope.collection = data;
+					 $scope.collectionCopy = angular.copy($scope.collection);
+				 });
+			 }
 
 			 $scope.keyWordChange = function () {
 				 $scope.collection = desktopDataService.filterCollection($scope.keyWord, $scope.collectionCopy);
 				 $timeout(function () {
 					 $scope.$apply();
 				 });
-
 			 };
+
+			 init();
 
 		 }]);
 })(angular);
