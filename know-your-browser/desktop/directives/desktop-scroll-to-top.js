e471d4d6d6fac7e88eb26ec6358b0a25b6adf541
Jackey-Sparrow <1109952026@qq.com>
2015-12-08 21:05:10 +0800

    add scroll to top

--- know-your-browser/desktop/directives/desktop-scroll-to-top.js
+++ know-your-browser/desktop/directives/desktop-scroll-to-top.js
@@ -1,6 +1,28 @@
 /**
  * Created by Jackey Li on 15/12/8.
  */
-(function () {
-    
+(function (angular) {
+    'use strict';
+
+    angular.module('desktop').directive('desktopScrollToTop',
+        [function () {
+            return {
+                restrict: 'EA',
+                scope: {
+                    content: '@'
+                },
+                template: '<div class="scrollToTop">{{::content}}</div>',
+                link: function (scope, element) {
+
+                    element.bind('click', function () {
+                        //chrome works fine
+                        document.body.scrollTop = 0;
+                    });
+
+                    scope.$on('$destroy', function () {
+                        element.unbind('click');
+                    });
+                }
+            };
+        }]);
 })(angular);
