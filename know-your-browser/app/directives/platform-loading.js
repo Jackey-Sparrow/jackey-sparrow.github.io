073b6353717b14cbda4b4f9112419b977ca4027e
Jackey.Sparrow <1109952026@qq.com>
2015-12-10 22:21:53 +0800

    scope destroy

--- know-your-browser/app/directives/platform-loading.js
+++ know-your-browser/app/directives/platform-loading.js
@@ -5,38 +5,46 @@
 	'use strict';
 
 	angular.module('platform').directive('platformLoading',
-		[
-			function () {
-				return {
-					restrict: 'EA',
-					replace: true,
-					template: '<div class="loading-container">' +
-					'<div class="loading-overlay"></div>' +
-					'<div class="loading-body"></div>' +
-					'<div class="loading-close">' +
-					'<div class="loading-close-text" data-ng-click="close()">x</div>' +
-					'</div>' +
-					'</div>',
-					link: function (scope) {
-						scope.close = function () {
-							scope.cancel();
-						};
-					}
-				};
-			}]);
+		[function () {
+			return {
+				restrict: 'EA',
+				replace: true,
+				template: '<div class="loading-container">' +
+				'<div class="loading-overlay"></div>' +
+				'<div class="loading-body"></div>' +
+				'<div class="loading-close">' +
+				'<div class="loading-close-text" data-ng-click="close()">x</div>' +
+				'</div>' +
+				'</div>',
+				link: function (scope) {
+					scope.close = function () {
+						scope.cancel();
+					};
+				}
+			};
+		}]);
 
 	angular.module('platform').factory('$platformLoading',
 		['$rootScope', '$compile', '$document',
 		 function ($rootScope, $compile, $document) {
 			 return {
 				 show: function () {
-					 var scope = $rootScope.$new(true);
-					 var element = scope.element = $compile('<platform-loading></platform-loading>')(scope);
-					 $document[0].body.appendChild(element[0]);
+
+					 var scope = $rootScope.$new(true),
+						 element;
+
+					 scope.showLoading = function () {
+						 element = scope.element = $compile('<platform-loading></platform-loading>')(scope);
+						 $document[0].body.appendChild(element[0]);
+					 };
 
 					 scope.cancel = function () {
 						 $document[0].body.removeChild(element[0]);
+						 element = null;
+						 scope.$destroy();
 					 };
+
+					 scope.showLoading();
 
 					 return scope.cancel;
 				 }
