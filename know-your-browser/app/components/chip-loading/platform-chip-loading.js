81fe38ec004572fce2c36531591e11b9a956abf6
Jackey.Sparrow <1109952026@qq.com>
2015-12-15 21:02:24 +0800

    chip-loading

---
+++ know-your-browser/app/components/chip-loading/platform-chip-loading.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Jackey Li on 2015/12/15.
+ */
+(function (angular) {
+	'use strict';
+
+	angular.module('platform').directive('platformChipLoading',
+		[function () {
+			return {
+				restrict: 'EA',
+				replace: true,
+				scope: {
+					dialogLoading: '='
+				},
+				templateUrl: 'app/components/chip-loading/templates/chip-loading.html',
+				link: function (scope, element) {
+					element.parent().css('position', ' relative');
+					function show(){
+
+					}
+
+				}
+			};
+		}]);
+})(angular);
